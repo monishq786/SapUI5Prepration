414e4e1b9fe050b335b3a3d6c966f06c95c61db2
Moh Monish <mohdmonish8882@gmail.com>
2025-02-26 16:57:04 +0000

    changes for jsonmodel

---
+++ app/project1/webapp/controller/JsonModel.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+], (Controller, JSONModel) => {
+    "use strict";
+    return Controller.extend("project1.controller.JsonModel", {
+        onInit: function () {
+            let oModel = new sap.ui.model.json.JSONModel();
+            oModel.loadData("model/employee.json");
+            this.getView().setModel(oModel, "EmpModel");
+        }
+    })
+})
